4d2157c40d94e56671d3fb3243e1541c2325f348
xuyunchun <758418945@qq.com>
2016-03-03 21:15:27 +0800

    pic

--- Spider/requests_spider/picdownloader.py
+++ Spider/requests_spider/picdownloader.py
@@ -11,42 +11,58 @@ import re
 # html = f.read()
 # f.close()
 # print html
-url = "https://otc.cbex.com/page/s/search/index"
-# url = 'https://www.crowdfunder.com/browse/deals'
-html = requests.get(url).text
-# print html
-
-    # <div class="bdlist_img"><a href="/prj/detail/2178.html" target="_blank">
-    # <img src="/editorUpload/image/2016/02/IMIMG-2016020023-3811/1456126240016039194-logo.jpg_MzAwLDMwMA==.jpg"/> </a>
-urls_field = re.findall('<div class="bdlist_img">(.*?)</a>',html,re.S)
-pic_urls = []
-for field in urls_field:
-    pic_url = re.findall('<img src="(.*?)"/>',field,re.S)
-    # new_full_url = urlparse.urljoin("https://otc.cbex.com",pic_url[0])
-    # print pic_url
-    pic_urls.append(pic_url)
-print pic_urls
-i = 0
-for each in pic_urls:
-    new_full_url = urlparse.urljoin("https://otc.cbex.com",each[0])
-    print "now downloading:" + str(i)
-    pic = requests.get(new_full_url)
-    fp = open('pic\\'+str(i)+'.jpg','wb')
-    fp.write(pic.content)
-    fp.close()
-    i += 1
 
 
-
-# class PicDownLoader(object):
-#     def getpictures(self, url):
-#         html = requests.get(url)
-#         html.encoding = 'utf-8'
-#         print html.text
+# url = "https://otc.cbex.com/page/s/search/index"
+# #获取网页源码
+# html = requests.get(url).text
+# '''
+# 获取图片地址链接
+# '''
+# urls_field = re.findall('<div class="bdlist_img">(.*?)</a>',html,re.S)
+# pic_urls = []
+# for field in urls_field:
+#     pic_url = re.findall('<img src="(.*?)"/>',field,re.S)
+#     pic_urls.append(pic_url)
+# print pic_urls
+# '''
+# 拼接完整地址，下载图片
+# '''
+# i = 0
+# for each in pic_urls:
+#     new_full_url = urlparse.urljoin("https://otc.cbex.com",each[0])
+#     print "now downloading:" + str(i)
+#     pic = requests.get(new_full_url)
+#     fp = open('pic\\'+str(i)+'.jpg','wb')
+#     fp.write(pic.content)
+#     fp.close()
+#     i += 1
 
 
 
-# if __name__ =="__main__":
-# 	url = "https://otc.cbex.com/page/s/search/index"
-# 	pic_downloader = PicDownLoader()
-# 	pic_downloader.getpictures(url)
+class PicDownLoader(object):
+    def getpictures(self, url):
+        #获取网页源码
+        html = requests.get(url).text
+        urls_field = re.findall('<div class="bdlist_img">(.*?)</a>',html,re.S)
+        pic_urls = []
+        for field in urls_field:
+            pic_url = re.findall('<img src="(.*?)"/>',field,re.S)
+            pic_urls.append(pic_url)
+        # print pic_urls
+        i = 0
+        for each in pic_urls:
+            new_full_url = urlparse.urljoin("https://otc.cbex.com",each[0])
+            print "now downloading:" + str(i)
+            pic = requests.get(new_full_url)
+            fp = open('pic\\'+str(i)+'.jpg','wb')
+            fp.write(pic.content)
+            fp.close()
+            i += 1
+
+
+
+if __name__ =="__main__":
+	url = "https://otc.cbex.com/page/s/search/index"
+	pic_downloader = PicDownLoader()
+	pic_downloader.getpictures(url)
